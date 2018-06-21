b36c56671a904a5f9aaebf4ad9954b5088281146
Ocelot5836 <FinntheRaider@gmail.com>
2018-06-20 20:08:38 -0700

    added game class

---
+++ src/main/java/com/ocelot/Game.java
@@ -0,0 +1,101 @@
+package com.ocelot;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.ocelot.util.Loader;
+
+public class Game implements Runnable {
+
+	private static Logger logger = LogManager.getLogger();
+	private static Game instance = new Game();
+
+	private boolean running;
+
+	private Game() {
+	}
+	
+	public synchronized void start() {
+		if (running)
+			return;
+
+		running = true;
+	}
+
+	public synchronized void stop() {
+		if (running)
+			return;
+
+		running = false;
+	}
+	
+	private void init() throws Exception {
+		
+	}
+
+	@Override
+	public void run() {
+		try {
+			init();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0D;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0.0D;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				update();
+				delta--;
+			}
+			if (running) {
+				render();
+				frames++;
+			}
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				logger.info("World Finished Loading: " + /* Game.worldFinishedLoading + */" --- FPS: " + frames);
+				frames = 0;
+			}
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		cleanUp();
+	}
+
+	private void update() {
+
+	}
+
+	private void render() {
+
+	}
+
+	private void cleanUp() {
+		Loader.cleanUp();
+	}
+
+	public static Logger logger() {
+		return logger;
+	}
+	
+	public static Game getInstance() {
+		return instance;
+	}
+	
+	public static void main(String[] args) {
+		new Thread(Game.getInstance()).start();
+		Game.getInstance().start();
+	}
+}
