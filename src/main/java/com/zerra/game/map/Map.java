c80de7721390fd777f4b59ebc4f82e80ded3ef1e
Hypeirochus <Hypeirochus@gmail.com>
2018-06-21 15:57:18 -0400

    Re-added tile loading code.

--- src/main/java/com/zerra/game/map/Map.java
+++ src/main/java/com/zerra/game/map/Map.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
+import org.joml.Vector3f;
+
 import com.zerra.Game;
 import com.zerra.game.world.tile.Tile;
+import com.zerra.game.world.tile.TileGrass;
 import com.zerra.game.world.tile.TileRock;
 import com.zerra.gfx.FrustumCullingFilter;
 import com.zerra.gfx.renderer.MasterRenderer;
@@ -50,53 +53,53 @@ public class Map {
 			}
 		}
 
-		// if (hasRemovedTiles) {
-		// Vector3f cameraPosition = camera.getPosition();
-		// Vector3f cameraLastPosition = camera.getLastPosition();
-		// float x = cameraPosition.x;
-		// float y = cameraPosition.y;
-		// float lastX = cameraLastPosition.x;
-		// float lastY = cameraLastPosition.y;
-		// if (x - lastX < 0) {
-		// for (int tileX = 1; tileX < 3; tileX++) {
-		// for (int tileY = -1; tileY < height + 1; tileY++) {
-		// int xPos = (int) (tileX + (Math.ceil(x - 64) / 16));
-		// int yPos = (int) (y / 16 + tileY);
-		// tiles.add(new TileGrass(xPos * 16, yPos * 16));
-		// }
-		// }
-		// }
-		//
-		// if (x - lastX > 0) {
-		// for (int tileX = 1; tileX < 3; tileX++) {
-		// for (int tileY = -1; tileY < height + 1; tileY++) {
-		// int xPos = (int) (tileX + width + (Math.ceil(x - 64) / 16));
-		// int yPos = (int) (y / 16 + tileY);
-		// tiles.add(new TileGrass(xPos * 16, yPos * 16));
-		// }
-		// }
-		// }
-		//
-		// if (y - lastY < 0) {
-		// for (int tileX = -1; tileX < width + 1; tileX++) {
-		// for (int tileY = 1; tileY < 3; tileY++) {
-		// int xPos = (int) (x / 16 + tileX);
-		// int yPos = (int) Math.ceil(tileY + y / 16);
-		// tiles.add(new TileGrass(xPos * 16, yPos * 16));
-		// }
-		// }
-		// }
-		//
-		// if (y - lastY > 0) {
-		// for (int tileX = -1; tileX < width + 1; tileX++) {
-		// for (int tileY = 1; tileY < 3; tileY++) {
-		// int xPos = (int) (x / 16 + tileX);
-		// int yPos = (int) Math.floor(tileY + height + y / 16);
-		// tiles.add(new TileGrass(xPos * 16, yPos * 16));
-		// }
-		// }
-		// }
-		// }
+		if (hasRemovedTiles) {
+			Vector3f cameraPosition = camera.getPosition();
+			Vector3f cameraLastPosition = camera.getLastPosition();
+			float x = cameraPosition.x;
+			float y = cameraPosition.y;
+			float lastX = cameraLastPosition.x;
+			float lastY = cameraLastPosition.y;
+			if (x - lastX < 0) {
+				for (int tileX = 1; tileX < 3; tileX++) {
+					for (int tileY = -1; tileY < height + 1; tileY++) {
+						int xPos = (int) (tileX + (Math.ceil(x - 64) / 16));
+						int yPos = (int) (y / 16 + tileY);
+						tiles.add(new TileGrass(xPos * 16, yPos * 16));
+					}
+				}
+			}
+
+			if (x - lastX > 0) {
+				for (int tileX = 1; tileX < 3; tileX++) {
+					for (int tileY = -1; tileY < height + 1; tileY++) {
+						int xPos = (int) (tileX + width + (Math.ceil(x - 64) / 16));
+						int yPos = (int) (y / 16 + tileY);
+						tiles.add(new TileGrass(xPos * 16, yPos * 16));
+					}
+				}
+			}
+
+			if (y - lastY < 0) {
+				for (int tileX = -1; tileX < width + 1; tileX++) {
+					for (int tileY = 1; tileY < 3; tileY++) {
+						int xPos = (int) (x / 16 + tileX);
+						int yPos = (int) Math.ceil(tileY + y / 16);
+						tiles.add(new TileGrass(xPos * 16, yPos * 16));
+					}
+				}
+			}
+
+			if (y - lastY > 0) {
+				for (int tileX = -1; tileX < width + 1; tileX++) {
+					for (int tileY = 1; tileY < 3; tileY++) {
+						int xPos = (int) (x / 16 + tileX);
+						int yPos = (int) Math.floor(tileY + height + y / 16);
+						tiles.add(new TileGrass(xPos * 16, yPos * 16));
+					}
+				}
+			}
+		}
 	}
 
 	public void render(MasterRenderer renderer) {
