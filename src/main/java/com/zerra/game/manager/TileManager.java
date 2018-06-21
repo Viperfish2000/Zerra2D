82796a49e03577732cf40a8068e7121b86260316
Ocelot5836 <FinntheRaider@gmail.com>
2018-06-21 14:12:18 -0700

    removed tile manager

---
+++ src/main/java/com/zerra/game/manager/TileManager.java
@@ -0,0 +1,46 @@
+package com.zerra.game.manager;
+
+import java.awt.Graphics;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import com.zerra.Game;
+import com.zerra.game.world.tile.Tile;
+import com.zerra.game.world.tile.TileGrass;
+
+
+public class TileManager {
+
+	private static CopyOnWriteArrayList<Tile> tilesToRender = new CopyOnWriteArrayList<Tile>();
+	
+	public static CopyOnWriteArrayList<Tile> getTiles() {
+		return tilesToRender;
+	}
+	
+	public void createTile(Tile tile) {
+		if(tile == null) {
+			tile = new TileGrass(0, 0);
+		}
+		tilesToRender.add(tile);
+	}
+	
+	public void destroyTile(Tile Tile) {
+		tilesToRender.remove(Tile);
+	}
+	
+	public void tick() {
+		for (Tile tile : tilesToRender) {
+			tile.update();
+		}
+	}
+	
+	public void render(Graphics g) {
+		for (Tile tile : tilesToRender) {
+			if(tile.getY() < Game.HEIGHT - 32 && tile.getY() > 0 - 32 && tile.getX() < Game.WIDTH && tile.getX() > 0 - 32) {
+				tile.render(g);
+			}else {
+				tilesToRender.remove(tile);
+				tile = null;
+			}
+		}
+	}
+}
