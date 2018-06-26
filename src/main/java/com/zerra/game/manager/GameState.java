a2633eec9fb40d60b39e2cbf9f229c98fe633b43
Ocelot5836 <FinntheRaider@gmail.com>
2018-06-25 21:13:26 -0700

    formatted GameState and added parameters to render

--- src/main/java/com/zerra/game/manager/GameState.java
+++ src/main/java/com/zerra/game/manager/GameState.java
@@ -1,38 +1,34 @@
 package com.zerra.game.manager;
 
+import javax.swing.Renderer;
+
 public class GameState {
 
 	private String stateName;
-	
+
 	public GameState(String name) {
 		this.stateName = name;
 	}
-	
+
 	public String getStateName() {
 		return stateName;
 	}
-	
+
 	public void update() {
-		
 	}
 
-	public void render() {
-		
+	public void render(Renderer renderer, double mouseX, double mouseY, float partialTicks) {
 	}
-	
+
 	public void onKeyPressed() {
-		
 	}
-	
+
 	public void onKeyReleased() {
-		
 	}
-	
+
 	public void onMousePressed() {
-		
 	}
-	
+
 	public void onMouseReleased() {
-		
 	}
 }
